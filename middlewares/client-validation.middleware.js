18e6cabb9b9c9c22a03e06f2ddb71258e6da9600
kevin <kevinrambalason1@gmail.com>
2022-03-09 20:14:38 +0100

    validations done on clients CRUD operations

---
+++ middlewares/client-validation.middleware.js
@@ -0,0 +1,44 @@
+const Joi = require("joi")
+
+const clientsMiddleware = {
+    validateGetClientById: (req, res, next) => {
+        const idSchema = Joi.string().hex().min(24).required()
+
+        const result = idSchema.validate(req.params.id)
+
+        if(result.error) {
+            return res.status(500).json(result.error.details)
+        }
+        return next()
+    },
+
+    validateAddClient: (req, res, next) => {
+        const clientSchema = Joi.object({
+            name: Joi.string().alphanum().max(30).required(),
+            phone_number: Joi.string().length(10).pattern(/^[0-9]+$/, 'numbers')
+        })
+
+        const result = clientSchema.validate(req.body)
+
+        if(result.error) {
+            return res.status(500).json(result.error.details)
+        }
+        return next()
+    },
+
+    validateEditClient: (req, res, next) => {
+        const clientSchema = Joi.object({
+            name: Joi.string().alphanum().max(30),
+            phone_number: Joi.string().length(10).pattern(/^[0-9]+$/, 'numbers')
+        })
+
+        const result = clientSchema.validate(req.body)
+
+        if(result.error) {
+            return res.status(500).json(result.error.details)
+        }
+        return next()
+    }
+}
+
+module.exports = { clientsMiddleware }
--- routes/client.route.js
+++ routes/client.route.js
@@ -1,13 +1,14 @@
 const router = require("express").Router()
 const { getAllClients, getClient, createClient, editClient, deleteClient } = require("../controllers/client.controller").clientController
+const { validateGetClientById, validateAddClient, validateEditClient } = require("../middlewares/client-validation.middleware").clientsMiddleware
 
 router.get("/", getAllClients)
-router.get("/:id", getClient)
+router.get("/:id", validateGetClientById, getClient)
 
-router.post("/add", createClient)
+router.post("/add", validateAddClient, createClient)
 
-router.put("/edit/:id", editClient)
+router.put("/edit/:id", validateGetClientById, validateEditClient, editClient)
 
-router.delete("/delete/:id", deleteClient)
+router.delete("/delete/:id", validateGetClientById, deleteClient)
 
 module.exports = router
